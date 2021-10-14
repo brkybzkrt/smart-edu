03740c2ea23710fa890bf59736f33c9e3e12b936
brkybzkrt <b9bozkurt959@gmail.com>
2021-10-14 13:34:51 +0300

    static files added and some pages edited

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const ejs =require('ejs');
 
 const app = express();
 
+
+
+//Template engine
+app.set('view engine','ejs');
+
+
+
+//Middlewares
+app.use(express.static('public'));
+
+
+
 const port = 3000;
+
 
 app.get('/',(req,res)=>{
 
-  res.status(200).send('hello world');
+  res.status(200).render('index',{page_name:"index"})
+})
+
+
+app.get('/about',(req,res)=>{
+
+  res.status(200).render('about',{page_name:"about"})
 })
 
 app.listen(port, () => {
