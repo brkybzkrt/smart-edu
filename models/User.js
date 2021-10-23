076aeec79e8668df1686e1d243882b391a3f6627
brkybzkrt <b9bozkurt959@gmail.com>
2021-10-23 15:49:46 +0300

    added isModified to password of user

--- models/User.js
+++ models/User.js
@@ -17,6 +17,9 @@ const UserSchema= new Schema({
 
 UserSchema.pre('save',function (next) {
     const user = this;
+
+    if(!user.isModified('password')) return next();
+    
     bcrypt.hash(user.password,10,(err,hash)=>{
         user.password=hash
         next();
