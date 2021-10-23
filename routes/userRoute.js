bd5d9661546c514ecf51dbbc6b6b908335e2cd0e
brkybzkrt <b9bozkurt959@gmail.com>
2021-10-23 14:55:17 +0300

    express-validator added and authController edited

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,22 @@
 const authController = require('../controllers/authController');
 const express = require('express');
 const authMiddleware = require('../middlewares/authMiddleware');
-
+const { body } = require('express-validator');
+const User =require('../models/User');
 
 const router = express.Router();
 
-router.route('/register').post(authController.createUser); // http://localhost:3000/users/register
+router.route('/register').post([
+    body('name').not().isEmpty().withMessage('Name field is not empty'),
+    body('email').isEmail().withMessage('Please enter correct email').custom((email)=>{
+        return User.findOne({email}).then(user=>{
+            if(user){
+                return Promise.reject('Email is already exist');
+            }
+        })
+    }),
+    body('password').not().isEmpty().withMessage('Please enter a password')
+],authController.createUser); // http://localhost:3000/users/register
 
 router.route('/login').post(authController.loginUser);
 
